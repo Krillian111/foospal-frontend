b4e297882a7cdd12c91cc1c1dfc2f98c8ce4f7fa
Krillian <krillian@gmx.de>
2020-05-02 19:27:14 +0200

    remove comments

--- src/components/common/SimpleTable.js
+++ src/components/common/SimpleTable.js
@@ -5,30 +5,22 @@ import TableRow from './TableRow';
 
 const Header = styled.h2`
   font-size: 1.5em;
-  /* was heißt em? */
 `;
 
 const Table = styled.div`
   width: max-content;
-  /* hier wollen wir noch modifizieren - Spaltenbreite */
   display: table;
   background-color: lightgrey;
   overflow: hidden;
   border-radius: 15px;
   border-width: 5px;
   border-style: solid;
-  /* grauer Rand außenrum */
   border-collapse: separate;
   border-spacing: 5px 3px;
-  /* graue Linien zwischen den Zeilen / Spalten */
   line-height: 2;
-  /* kann eigtl weg, wenn wir es geschafft haben, dass padding funktioniert */
-  /*margin: 0 100px;*/
-  /*rückt die Tabelle ein bisschen nach rechts ein - die null rückt sie um null nach unten*/
 `;
 
 const TableHeaderRow = styled.div`
-  /* Überschriftenzeile in der Tabelle */
   display: table-row;
   font-size: 1.2em;
   background-color: #be8595;
@@ -37,11 +29,9 @@ const TableHeaderRow = styled.div`
   color: rgba(0, 128, 0, 1);
   line-height: 2;
   white-space: nowrap;
-  /* kein Zeilenumbruch - dadurch Spalten breiter */;
-  `;
+`;
 
 const StyledTableRow = styled.div`
-  /* Zeilen der Tabelle */
   display: table-row;
   background-color: ${props => props.rowIndex % 2 === 1 ? '#adbe85' : '#5f8059'};
   border: 2px solid lightgrey;
@@ -58,7 +48,6 @@ function SimpleTable({ title, columnConfigs, tableHeaders, dataRows }) {
       throw new Error(`Error, received ${columnConfigs.length} column configs, ${tableHeaders.length} headers and ${dataRows[0].length} rows`);
     }
   }
-/* sandra: was bedeutet diese function? */
 
   return(
     <div>
