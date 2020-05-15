badd734374c0fb8d4b16c6e1fd6c782b4f5e4d02
Krillian <krillian@gmx.de>
2020-05-15 21:19:36 +0200

    fix broken mapDispatchToProps

--- src/containers/FetchSinglesButton.js
+++ src/containers/FetchSinglesButton.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import { fetchSingles } from '../store/actions/stats/singles/fetchSingles';
 import PrimaryButton from '../components/0atom/button/PrimaryButton';
 
-const mapDispatchToProps = {
-    label: 'Fetch data',
-    onClick: fetchSingles,
-};
+function mapDispatchToProps(dispatch) {
+    return {
+        label: 'Fetch data',
+        onClick: () => dispatch(fetchSingles()),
+    };
+}
 
 export default connect(null, mapDispatchToProps)(PrimaryButton);
